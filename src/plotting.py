f561be24c7ba4ba5abba0aea41c9f704aa705df5
Shreshth Tuli <shreshthtuli@gmail.com>
2021-03-03 22:06:34 +0530

    Update plotting.py

--- src/plotting.py
+++ src/plotting.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 import statistics
 import os
 import numpy as np
 
 plt.style.use(['science', 'ieee'])
-plt.rcParams["text.usetex"] = True
+plt.rcParams["text.usetex"] = False
 plt.rcParams['figure.figsize'] = 6, 2
 
 os.makedirs('plots', exist_ok=True)
@@ -16,13 +17,20 @@ def smooth(y, box_pts=1):
 
 def plotter(name, y_true, y_pred, ascore, labels):
 	os.makedirs(os.path.join('plots', name), exist_ok=True)
+	pdf = PdfPages(f'plots/{name}/output.pdf')
 	for dim in range(y_true.shape[1]):
 		y_t, y_p, l, a_s = y_true[:, dim], y_pred[:, dim], labels[:, dim], ascore[:, dim]
 		fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-		ax1.plot(smooth(y_t), label='True')
-		ax1.plot(smooth(y_p), '-', alpha=0.6, label='Predicted')
-		ax1.plot(l, '--', alpha=0.5)
-		ax1.legend()
-		ax2.plot(smooth(a_s))
-		fig.savefig(f'plots/{name}/{dim}.pdf')
+		ax1.set_ylabel('Value')
+		ax1.set_title(f'Dimension = {dim}')
+		ax1.plot(smooth(y_t), linewidth=0.2, label='True')
+		ax1.plot(smooth(y_p), '-', alpha=0.6, linewidth=0.3, label='Predicted')
+		ax1.plot(l, '--', linewidth=0.3, alpha=0.5)
+		ax1.fill_between(np.arange(l.shape[0]), l, color='blue', alpha=0.3)
+		if dim == 0: ax1.legend(ncol=2, bbox_to_anchor=(0.6, 1.02))
+		ax2.plot(smooth(a_s), linewidth=0.2, color='g')
+		ax2.set_xlabel('Timestamp')
+		ax2.set_ylabel('Anomaly Score')
+		pdf.savefig(fig)
 		plt.close()
+	pdf.close()
