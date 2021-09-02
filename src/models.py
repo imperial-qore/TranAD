14a4cb3b7064b49daddcec9e9a8882ef18feabb4
Shreshth Tuli <shreshthtuli@gmail.com>
2021-09-02 20:15:27 +0530

    Hyperparameter tuning

--- src/models.py
+++ src/models.py
@@ -232,16 +232,18 @@ class CAE_M(nn.Module):
 	def __init__(self, feats):
 		super(CAE_M, self).__init__()
 		self.name = 'CAE_M'
-		self.lr = 0.00005
+		self.lr = 0.001
 		self.n_feats = feats
 		self.n_window = feats
 		self.encoder = nn.Sequential(
-			nn.Conv2d(1, 64, (1, 1), 1), nn.ReLU(True),
-			nn.Conv2d(64, 128, (1, 1), 1), nn.ReLU(True),
+			nn.Conv2d(1, 8, (3, 3), 1, 1), nn.Sigmoid(),
+			nn.Conv2d(8, 16, (3, 3), 1, 1), nn.Sigmoid(),
+			nn.Conv2d(16, 32, (3, 3), 1, 1), nn.Sigmoid(),
 		)
 		self.decoder = nn.Sequential(
-			nn.ConvTranspose2d(128, 64, (3, 3), 1, 1), nn.ReLU(True),
-			nn.ConvTranspose2d(64, 1, (3, 3), 1, 1), nn.Sigmoid(),
+			nn.ConvTranspose2d(32, 4, (3, 3), 1, 1), nn.Sigmoid(),
+			nn.ConvTranspose2d(4, 4, (3, 3), 1, 1), nn.Sigmoid(),
+			nn.ConvTranspose2d(4, 1, (3, 3), 1, 1), nn.Sigmoid(),
 		)
 
 	def forward(self, g):
